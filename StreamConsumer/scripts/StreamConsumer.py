ddb93bb724eb7d2e660aa0c10a53160f1a1dcb0f
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-10-18 16:32:44 +0800

    Created super class StreamConsumer and added SupplyStreamConsumer
    - Subclassed DemandStreamConsumer to inherit StreamConsumer
    - Added SupplyStreamConsumer to consume supplier stream
    	- Stream is subclassed to StreamConsumer

---
+++ StreamConsumer/scripts/StreamConsumer.py
@@ -0,0 +1,20 @@
+from KinesisClient import KinesisClient
+from datetime import datetime, timedelta
+from dateutil.tz import tzoffset
+
+class StreamConsumer(object):
+	client 	= None
+
+	def __init__(self):
+		return
+
+	def consumeFirstInstance(self, stream_name, time_stamp):
+		stream_desc = self.client.describe_stream(StreamName=stream_name)
+		shard_id 	= stream_desc['StreamDescription']['Shards'][0]['ShardId']
+		shard_itr 	= self.client.get_shard_iterator(StreamName=stream_name, 
+													 ShardId="shardId-000000000001", 
+													 ShardIteratorType="AT_TIMESTAMP",
+													 Timestamp=time_stamp)['ShardIterator']
+
+		response 	= self.client.get_records(ShardIterator=shard_itr)
+		return response
--- StreamConsumer/scripts/SupplyStreamConsumer.py
+++ StreamConsumer/scripts/SupplyStreamConsumer.py
@@ -3,30 +3,30 @@ from datetime import datetime, timedelta
 from dateutil.tz import tzoffset
 from StreamConsumer import StreamConsumer
 
-class DemandStreamConsumer(StreamConsumer):
+class SupplyStreamConsumer(StreamConsumer):
 	client 	= None
 
 	def __init__(self, config_path):
-		super(DemandStreamConsumer, self).__init__()
+		super(SupplyStreamConsumer, self).__init__()
 		self.client = KinesisClient(config_path).connect()
 		return
 
 	def consume(self, stream_name, from_time_stamp):
-		dd_count 	= 0
+		ss_count 	= 0
 		response 	= self.consumeFirstInstance(stream_name, from_time_stamp)
 		result 		= response['Records']
 
 		while("NextShardIterator" in response and result):
-			dd_count 		+= len(result)
+			ss_count 		+= len(result)
 			next_shard_itr 	= response['NextShardIterator']
 			response 		= self.client.get_records(ShardIterator=next_shard_itr)
 			result 			= response['Records']
-		return dd_count
+		return ss_count
 
 if __name__ == "__main__":
 	import os
 	config_path = os.path.join('..','config','config.xml')
-	consumer 	= DemandStreamConsumer(config_path)
-	resp 		= consumer.consume("grab-demand-stream", datetime(2017,10,17,19,51,50, 
+	consumer 	= SupplyStreamConsumer(config_path)
+	resp 		= consumer.consume("grab-supply-stream", datetime(2017,10,17,19,51,50, 
 									tzinfo=tzoffset("Offset", timedelta(hours=8))))
 	print(resp)

7a0405341032c17c7e51099340aab8864ecf7c89
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-10-18 16:44:50 +0800

    Minor changes to all Stream Consumers
    - Added print functions for start and end of consuming stream

--- StreamConsumer/scripts/SupplyStreamConsumer.py
+++ StreamConsumer/scripts/SupplyStreamConsumer.py
@@ -16,6 +16,7 @@ class SupplyStreamConsumer(StreamConsumer):
 		response 	= self.consumeFirstInstance(stream_name, from_time_stamp)
 		result 		= response['Records']
 
+		# Add supply processing logic here
 		while("NextShardIterator" in response and result):
 			ss_count 		+= len(result)
 			next_shard_itr 	= response['NextShardIterator']
@@ -27,6 +28,8 @@ if __name__ == "__main__":
 	import os
 	config_path = os.path.join('..','config','config.xml')
 	consumer 	= SupplyStreamConsumer(config_path)
+	print("Consuming stream data at time: {0}".format(str(datetime.now())))
+
 	resp 		= consumer.consume("grab-supply-stream", datetime(2017,10,17,19,51,50, 
 									tzinfo=tzoffset("Offset", timedelta(hours=8))))
-	print(resp)
+	print("Stream data consumed at time: {0}".format(str(datetime.now())))
--- StreamConsumer/scripts/TrafficLogStreamConsumer.py
+++ StreamConsumer/scripts/TrafficLogStreamConsumer.py
@@ -3,33 +3,33 @@ from datetime import datetime, timedelta
 from dateutil.tz import tzoffset
 from StreamConsumer import StreamConsumer
 
-class DemandStreamConsumer(StreamConsumer):
+class TrafficLogStreamConsumer(StreamConsumer):
 	client 	= None
 
 	def __init__(self, config_path):
-		super(DemandStreamConsumer, self).__init__()
+		super(TrafficLogStreamConsumer, self).__init__()
 		self.client = KinesisClient(config_path).connect()
 		return
 
 	def consume(self, stream_name, from_time_stamp):
-		dd_count 	= 0
+		ss_count 	= 0
 		response 	= self.consumeFirstInstance(stream_name, from_time_stamp)
 		result 		= response['Records']
 
-		# Add demand processing logic here
+		# Add traffic logs processing logic here
 		while("NextShardIterator" in response and result):
-			dd_count 		+= len(result)
+			ss_count 		+= len(result)
 			next_shard_itr 	= response['NextShardIterator']
 			response 		= self.client.get_records(ShardIterator=next_shard_itr)
 			result 			= response['Records']
-		return dd_count
+		return ss_count
 
 if __name__ == "__main__":
 	import os
 	config_path = os.path.join('..','config','config.xml')
-	consumer 	= DemandStreamConsumer(config_path)
+	consumer 	= TrafficLogStreamConsumer(config_path)
 	print("Consuming stream data at time: {0}".format(str(datetime.now())))
 
-	resp 		= consumer.consume("grab-demand-stream", datetime(2017,10,17,19,51,50, 
+	resp 		= consumer.consume("grab-traffic-log-stream", datetime(2017,10,17,19,51,50, 
 									tzinfo=tzoffset("Offset", timedelta(hours=8))))
 	print("Stream data consumed at time: {0}".format(str(datetime.now())))

c36e0444a0e26ad0a842bbc4fd3ccc5285b2d742
JKOK005 <JKOK005@e.ntu.edu.sg>
2017-10-24 23:02:24 +0800

    Added admin registration for client app
    - Created superuser JKOK005
    - Registered each model to an admin class

--- ClientAppViewer/Viewer/Webpage/admin.py
+++ ClientAppViewer/Viewer/Webpage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from Webpage.models import *
 
-# Register your models here.
+@admin.register(Geohash)
+class GeohashAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Surgeprice)
+class SurgepriceAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Trafficlog)
+class TrafficlogAdmin(admin.ModelAdmin):
+	pass
